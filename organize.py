4d77fc3b9a66d7e40a86a3956644dd8dd5e3f86c
Mayookh M <79088427+mayookhm@users.noreply.github.com>
2022-10-16 13:33:27 +0530

    Add files via upload

---
+++ organize.py
@@ -0,0 +1,10 @@
+import os
+import shutil
+
+from_dir="C:\Users\DELL\Downloads\C102_assets-main\C102_assets-main"
+to_dir="C:\Users\DELL\c102"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
+
+# Move All Image files from Downloads Folder to Another Folder
